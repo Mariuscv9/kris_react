998dd5a3be2529a1583da13e16580ade47689720
Marius Enache <enachemarius2012@gmail.com>
2023-08-09 23:00:56 +0300

    deploy

--- app/components/LastPosts.js
+++ app/components/LastPosts.js
@@ -1,5 +1,6 @@
 async function getImage() {
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}&limit=15`;
+  const url = asd
+  // `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}&limit=15`;
   const res = await fetch(url);
   const data = await res.json();
   return data;
