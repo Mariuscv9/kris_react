2122bc65a01d6054f8651b8614ad277cc26e9ce3
Marius Enache <enachemarius2012@gmail.com>
2023-08-23 17:56:38 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,15 +9,20 @@ const inter = Inter({ subsets: ["latin"] });
 <link rel="icon" href="/favicon.png" sizes="any" />;
 export const metadata = {
   title: "Salon de manichiura si pedichiura Bucuresti",
-  description:
-    "Ofer o gamă variată de tratamente pentru unghii, într-un ambient relaxant in Bucuresti. Descoperă servicii personalizate ce redau frumusețea unghiilor tale la cel mai înalt nivel.",
-  author: "Marius Enache",
+  author:"Marius Enache",
+  description:"Ofer o gamă variată de tratamente pentru unghii, într-un ambient relaxant in Bucuresti. Descoperă servicii personalizate ce redau frumusețea unghiilor tale la cel mai înalt nivel.",
   google_site_verification:"btxmvWxwgbC7H6r8MKPJywG2Nvi-_OtB4s5-j25Mjv0"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content={metadata.author} />
+        <meta name="google" content={metadata.google_site_verification} />
+      </Head>
       <body className={inter.className}>
         {children}
         <Footer />
