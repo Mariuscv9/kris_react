3435a0e64c4085eddbef55fde979638731915f9b
Marius Enache <enachemarius2012@gmail.com>
2023-08-23 17:01:49 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
-import 'dotenv/config'
-
+import "dotenv/config";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-<link rel="icon" href="/favicon.png" sizes="any" />
-export const metadata = {
-  title: "CK Nails Studio",
-  description: "Ofer o gamă variată de tratamente pentru unghii, într-un ambient relaxant in Bucuresti. Descoperă servicii personalizate ce redau frumusețea unghiilor tale la cel mai înalt nivel.",
-  keywords:"manichiura, pedichiura, unghii",
-  author:"Marius Enache"
-};
+<link rel="icon" href="/favicon.png" sizes="any" />;
+// export const metadata = {
+//   title: "CK Nails Studio",
+//   description:
+//     "Ofer o gamă variată de tratamente pentru unghii, într-un ambient relaxant in Bucuresti. Descoperă servicii personalizate ce redau frumusețea unghiilor tale la cel mai înalt nivel.",
+//   keywords: "manichiura, pedichiura, unghii",
+//   author: "Marius Enache",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Salon de manichiura si pedichiura Bucuresti</title>
+        <meta
+          name="description"
+          content="Ofer o gamă variată de tratamente pentru unghii, într-un ambient relaxant in Bucuresti. Descoperă servicii personalizate ce redau frumusețea unghiilor tale la cel mai înalt nivel."
+        />
+        <meta name="author" content="Marius Enache"/>
+        <meta name="google-site-verification" content="btxmvWxwgbC7H6r8MKPJywG2Nvi-_OtB4s5-j25Mjv0" />
+      </Head>
       <body className={inter.className}>
-        
         {children}
         <Footer />
       </body>
