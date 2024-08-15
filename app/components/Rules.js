17383afd4a15e5ced70c1f1cb3b3f82adb42c5aa
Marius Enache <enachemarius2012@gmail.com>
2024-08-15 19:04:13 +0300

    update

--- app/components/Rules.js
+++ app/components/Rules.js
@@ -2,100 +2,162 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion"; // Import Accordion component
+import Image from "next/image";
 import wapp from "/public/wappbtn.png";
 import insta from "/public/instagrambtn.png";
-import Image from "next/image";
 
 export default function Rules() {
   return (
-    <Container className="reguli">
+    <Container className="reguli mb-5">
       <span id="anchor4" className="anchor"></span>
       <Row>
         <Col md={6}>
-          {" "}
           <h3 className="fw-light text-center mt-5 mb-4">REGULI</h3>
-          <ul>
-            <li>
-              Timpul meu de lucru este de +/- 2 ore, în funcție de complexitate.
-              Am rugămintea să aveți răbdarea necesară pentru a vă putea oferi
-              manichiuri corecte din punct de vedere tehnic dar și cu un aspect
-              impecabil.
-            </li>
-            <li>
-              Vă rog să respectați termenul pentru întreținere de 3, maxim 4
-              săptămâni astfel încât unghiile voastre să rămână sănătoase în
-              continuare. Se aplică și la pedichiură!
-            </li>
-            <li>
-              Vă rog să nu întârziați si nu veniti mai repede la programare!
-              Daca avem programare la ora 10 de exemplu, va rog, veniti la ora
-              stabilita. Pauzele, chiar si de 10 minute, sunt importante pentru
-              mine. În cazul în care se întâmplă sa intarziati, din păcate, va
-              trebui să eliminăm anumite elemente din manichiură (design,
-              culoare). La întârzierile mai mari de 30 de minute, programarea va
-              fi anulată.
-            </li>
-            <li>
-              Programările se anulează cu cel puțin 24 ore înainte. La 2 anulari
-              consecutive colaborarea noastra se va incheia.
-            </li>
-            <li>
-              Daca anulati in aceeasi zi, la urmatoarea voastra programare, veti
-              plati si jumatate din serviciul anulat.
-            </li>
-            <li>
-              Vă rog să încercați să veniți cât de cât hotărâte în privința
-              modelului, culorii etc.
-            </li>
-            <li>
-              {" "}
-              NU execut tehnici / forme incorecte sau necompatibile cu patul
-              vostru unghial! Nu accept insistențe pe acest aspect.
-            </li>
-            <li>
-              {" "}
-              Având în vedere că salonul este micuț, am rugămintea să veniți
-              neînsoțite.
-            </li>
-            <li>Programările se fac din timp, minim o luna inainte!.</li>
-            <li>
-              Dacă doriți design neapărat, vă rog să mă anunțați atunci cand va
-              faceti programare pentru a vă putea aloca mai mult timp în
-              programator.
-            </li>
-          </ul>
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Timpul meu de lucru</Accordion.Header>
+              <Accordion.Body>
+                Timpul meu de lucru este de +/- 2 ore, în funcție de complexitate.
+                Am rugămintea să aveți răbdarea necesară pentru a vă putea oferi
+                manichiuri corecte din punct de vedere tehnic dar și cu un aspect
+                impecabil.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Respectați termenul pentru întreținere</Accordion.Header>
+              <Accordion.Body>
+                Vă rog să respectați termenul pentru întreținere de 3, maxim 4
+                săptămâni astfel încât unghiile voastre să rămână sănătoase în
+                continuare. Se aplică și la pedichiură!
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Nu întârziați și nu veniți mai repede la programare!</Accordion.Header>
+              <Accordion.Body>
+                Vă rog să nu întârziați și nu veniți mai repede la programare!
+                Dacă avem programare la ora 10, de exemplu, vă rog să veniți la ora
+                stabilită. Pauzele, chiar și de 10 minute, sunt importante pentru
+                mine. În cazul în care întârziați, din păcate, va trebui să eliminăm
+                anumite elemente din manichiură (design, culoare). La întârzierile
+                mai mari de 30 de minute, programarea va fi anulată.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Anulări</Accordion.Header>
+              <Accordion.Body>
+                Programările se anulează cu cel puțin 24 ore înainte. La 2 anulări
+                consecutive, colaborarea noastră se va încheia.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Anulări în aceeași zi</Accordion.Header>
+              <Accordion.Body>
+                Dacă anulați în aceeași zi, la următoarea voastră programare, veți
+                plăti și jumătate din serviciul anulat.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Modelul dorit</Accordion.Header>
+              <Accordion.Body>
+                Vă rog să încercați să veniți cât de cât hotărâte în privința
+                modelului, culorii etc.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="6">
+              <Accordion.Header>NU execut tehnici incorecte</Accordion.Header>
+              <Accordion.Body>
+                NU execut tehnici / forme incorecte sau necompatibile cu patul
+                vostru unghial! Nu accept insistențe pe acest aspect.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="7">
+              <Accordion.Header>Veniți neînsoțite</Accordion.Header>
+              <Accordion.Body>
+                Având în vedere că salonul este micuț, am rugămintea să veniți
+                neînsoțite.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="8">
+              <Accordion.Header>Programările se fac din timp</Accordion.Header>
+              <Accordion.Body>
+                Programările se fac din timp, minim o lună înainte!
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="9">
+              <Accordion.Header>Designul unghiilor</Accordion.Header>
+              <Accordion.Body>
+                Dacă doriți design neapărat, vă rog să mă anunțați atunci când vă
+                faceți programare pentru a vă putea aloca mai mult timp în
+                programator.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Col>
         <Col md={6}>
           <h3 className="fw-light text-center mt-5 mb-4">INFORMATII</h3>
-          <ul>
-            <li>
-              Cu o zi inainte de programare veti primi un mesaj de confirmare
-              pana la ora 21, am rugamintea sa raspundeti la acest mesaj
-              confirmand sau infirmand programarea; daca nu primesc raspuns la
-              acest mesaj, programarea va fi automat anulata.
-            </li>
-            <li>Daca aveti unghii lipsa, va rog sa ma anuntati.</li>
-            <li>
-              {" "}
-              Pentru programari sau alte nelămuriri raspund doar la mesaje, nu
-              pot vorbi la telefon in timp ce lucrez.
-            </li>
-            <li>
-              Nu fac constructii lungi (balerina, migdala ruseasca etc), doar
-              unghii mini si medii.
-            </li>
-            <li>
-              NU fac constructii pe unghii roase , doar protezare pentru a
-              reface patul unghial.
-            </li>
-            <li>
-              Atunci când doriți să vă faceți programare sau să cereți detalii,
-              o puteti face prin dm pe instagram accesand
-              butonul: 
-            </li>
-          </ul>
-          <div className="info-socials">
-            
+          <Accordion defaultActiveKey="6">
+          <Accordion.Item eventKey="6">
+              <Accordion.Header>Detalii pentru programare</Accordion.Header>
+              <Accordion.Body>
+                <h5 className="fw-bold">
+                  <span className="exclamare">!</span> Când doriți o programare, vă rog, să îmi lăsați următoarele detalii:
+                </h5>
+                <ul>
+                  <li>Numele complet;</li>
+                  <li>Data și ora la care vreți programare;</li>
+                  <li>Ce fel de serviciu vă doriți;</li>
+                  <li>O poză cu unghiile voastre actuale (în cazul în care veniți pentru prima oară)</li>
+                </ul>
+                <h5>
+                  <span className="exclamare">‼</span> Clientele noi plătesc un avans de 50% din valoarea serviciului
+                  programat. Avansul nu este returnabil decât în cazul anulării cu
+                  minim 24 de ore înainte.
+                </h5>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Confirmarea programării</Accordion.Header>
+              <Accordion.Body>
+                Cu o zi înainte de programare, veți primi un mesaj de confirmare
+                până la ora 21. Am rugămintea să răspundeți la acest mesaj
+                confirmând sau infirmând programarea; dacă nu primesc răspuns la
+                acest mesaj, programarea va fi automat anulată.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Unghii lipsă</Accordion.Header>
+              <Accordion.Body>
+                Dacă aveți unghii lipsă, vă rog să mă anunțați.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Programări și nelămuriri</Accordion.Header>
+              <Accordion.Body>
+                Pentru programări sau alte nelămuriri răspund doar la mesaje. Nu
+                pot vorbi la telefon în timp ce lucrez.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Construcții lungi</Accordion.Header>
+              <Accordion.Body>
+                Nu fac construcții lungi (balerina, migdala rusească etc.), doar
+                unghii mini și medii.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Construcții pe unghii roase</Accordion.Header>
+              <Accordion.Body>
+                NU fac construcții pe unghii roase, doar protezare pentru a
+                reface patul unghial.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Programări prin Instagram</Accordion.Header>
+              <Accordion.Body>
+                Atunci când doriți să vă faceți programare sau să cereți detalii,
+                o puteți face prin DM pe Instagram accesând butonul:
+                <div className="info-socials mt-4">
             <a
               className="nav-link"
               href="https://www.instagram.com/ck.manicure/"
@@ -103,25 +165,18 @@ export default function Rules() {
               <Image src={insta} alt="instagram" />
             </a>
           </div>
-          <ul className="pt-4">
-            <h5 className="fw-bold">
-              <span className="exclamare">!</span> Când doriți o programare, vă rog, să îmi lăsați următoarele
-              detalii:
-            </h5>
-            <li>Numele complet;</li>
-            <li>Data și ora la care vreți programare;</li>
-            <li>Ce fel de serviciu vă doriți;</li>
-            <li>
-              {" "}
-              O poza cu unghiile voastre actuale (în cazul în care veniți pentru
-              prima oară)
-            </li>
-            <h5>
-              <span className="exclamare">‼</span> Clientele noi platesc un avans de 50% din valoarea serviciului
-              programat. Avansul nu este returnabil decat in cazul anularii cu
-              minim 24 de ore inainte.
-            </h5>
-          </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+            
+          </Accordion>
+          {/* <div className="info-socials mt-4">
+            <a
+              className="nav-link"
+              href="https://www.instagram.com/ck.manicure/"
+            >
+              <Image src={insta} alt="instagram" />
+            </a>
+          </div> */}
         </Col>
       </Row>
     </Container>
